078c7ddbe8f9b4d4bf28b2b90e7d1bf782de85d6
Alexey Potapov <aloy@bk.ru>
2022-04-05 17:49:45 +0300

    ownerId field deleted from class Comment

--- src/main/kotlin/ru/netology/Comment.kt
+++ src/main/kotlin/ru/netology/Comment.kt
@@ -3,7 +3,6 @@ package ru.netology
 data class Comment(
     val id: Int = 0,
     val noteId: Int = 0,
-    val ownerId: Int = 0,
     val date: Int,
     val replyTo: Int = 0,
     val message: String,
